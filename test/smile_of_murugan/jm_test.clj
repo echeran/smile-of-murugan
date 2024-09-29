6a8a076957a0f1741a0a6d25e8305c1a4ff8ecb1
Elango Cheran <elango.cheran@gmail.com>
2024-09-29 09:02:33 -0700

    Add more exploratory unit tests

--- test/smile_of_murugan/jm_test.clj
+++ test/smile_of_murugan/jm_test.clj
@@ -13,12 +13,12 @@
           md-lines (jm/docai-json-to-md json-str)
           ]
       (->> md-lines
-           (take 50)
+           (take 150)
            (run! println))
       (is (= true false)))))
 
-(deftest json-style-test
-  (testing "Context of the test assertions"
+(deftest json-style-test-1
+  (testing "Extract italicized word Cankam"
     (let [json-file-name "sample/docai-doc-ocr.json"
           json-file (fs/file json-file-name)
           json-str (slurp json-file)
@@ -27,6 +27,22 @@
       (let [word1 (subs text 877 884)
             word2 (subs text 884 890)
             word3 (subs text 890 892)]
+        (prn "word1:" word1)
+        (prn "word2:" word2)
+        (prn "word3:" word3))
+      (is (= true false)))))
+
+(deftest json-style-test-2
+  (testing "Extract italicized word élite"
+    (let [json-file-name "sample/docai-doc-ocr.json"
+          json-file (fs/file json-file-name)
+          json-str (slurp json-file)
+          resp (json/parse-string json-str)
+          text (get resp "text")]
+      (let [word1 (subs text 1018 1021)
+            word2 (subs text 1021 1026)
+            word3 (subs text 1026 1028)]
+        (prn "(class text):" (class text))
         (prn "word1:" word1)
         (prn "word2:" word2)
         (prn "word3:" word3))
